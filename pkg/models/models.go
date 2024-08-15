293bca66bf4f63a2130a25d87fd58ae69a6c9f2b
Aryan <bhardwajaryan1000@gmail.com>
2024-08-15 16:25:49 +0530

    Updated Customer Struct

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,19 +1,24 @@
 package models
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
 
 type Customer struct {
-	bun.BaseModel `bun:"table:customer,alias:c"`
-	UID           uuid.UUID
-	Name          string
-	Age           int
-	Account       Account
-}
-
-type Account struct {
-	AccountNumber  int
-	CurrentBalance float32
+	bun.BaseModel   `bun:"table:customer,alias:c"`
+	UID             uuid.UUID
+	Name            string
+	Age             int
+	PrimaryEmail    string
+	SecondaryEmail  string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+	Status          string
+	AccountID       int
+	Address         int
+	ResourceVersion time.Time
+	Tags            []string
 }
